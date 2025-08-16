2b4c97a34c3dd2823a68fdd92a070f5dd47baabf
Oyage <oyage@mail.ru>
2025-08-16 11:42:56 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,46 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	runes := []rune(s)
+	ln := len(runes)
+	if ln == 0 {
+		return "", nil
+	}
+	if unicode.IsDigit(runes[0]) {
+		return "", ErrInvalidString
+	}
+	var b strings.Builder
+	var prev rune
+	var prevExists bool
+
+	for i := 0; i < ln; i++ {
+		r := runes[i]
+		if unicode.IsDigit(r) {
+			if !prevExists || unicode.IsDigit(prev) {
+				return "", ErrInvalidString
+			}
+			count := int(r - '0')
+			if count > 0 {
+				b.WriteString(strings.Repeat(string(prev), count))
+			}
+			prevExists = false
+			continue
+		}
+		if prevExists {
+			b.WriteRune(prev)
+		}
+		prev = r
+		prevExists = true
+	}
+	if prevExists {
+		b.WriteRune(prev)
+	}
+	return b.String(), nil
 }
